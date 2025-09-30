15554de84fcf792bbea49eeec3d571e7fb13a5a6
hassani rhahim <rhahim12@gmail.com>
2025-09-30 13:36:43 +0200

    ajout 3

--- 3 - dark mode/script.js
+++ 3 - dark mode/script.js
@@ -3,3 +3,44 @@ Nouvelles compétences:
 - forEach
 - querySelectorAll
 */
+const smiley = document.querySelector("i")
+console.log(smiley)
+
+const html = document.querySelector("html")
+console.log(html)
+
+smiley.addEventListener('click', () => {
+
+    smiley.classList.toggle('fa-moon')
+    // smiley.classList.toggle('darkmode')
+    // console.log("reussi")
+});
+
+div_elem = document.querySelector(".switch")
+console.log(div_elem)
+
+const class_all_arr = document.querySelectorAll("*")
+// console.log(class_all)
+
+class_all_arr.forEach(class_obj => {
+    // console.log(class_obj)
+    console.log(class_all_arr[13])
+    
+   
+
+
+    div_elem.addEventListener('click', () => {
+        div_elem.classList.toggle("darkmode")
+        // html.classList.toggle("darkmode");
+        class_obj.classList.toggle("darkmode")  
+        // smiley.classList.toggle("is-visible")
+        // class_all_arr[13].classList.toggle("fa-moon")
+        // console.log("test")
+
+
+
+
+
+
+    });
+})
