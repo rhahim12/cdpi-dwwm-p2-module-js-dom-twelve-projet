0eb83a567592e161dbb8c7af72be2cf25f62d524
hassani rhahim <rhahim12@gmail.com>
2025-09-30 14:44:20 +0200

    ajout 4

--- 4 - menuburger/script.js
+++ 4 - menuburger/script.js
@@ -3,3 +3,14 @@
 const burger = document.querySelector(".burger");
 // 2. Je recupere la fenetre modal
 const modal = document.querySelector(".modal");
+
+
+burger.addEventListener('click', () =>{
+    modal.classList.toggle("show-modal")
+    burger.classList.toggle("fa-x")
+    burger.classList.toggle("show-modal")
+
+
+})
+
+console.log(burger)
