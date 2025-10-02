441f5ac59bed3db9b424da86a11dd771ffd1d277
hassani rhahim <rhahim12@gmail.com>
2025-10-02 15:29:26 +0200

    ajout 8

--- 8 - Fiche produit/script.js
+++ 8 - Fiche produit/script.js
@@ -19,4 +19,35 @@ const panierMsg = document.querySelector(".panier-msg");
 // 5. Je crée une variable qui contient le nombre de produits ajoutées ua panier
 let panier = 0;
 
-// ...
+
+const p_elem = document.createElement("p")
+panierMsg.appendChild(p_elem)
+
+btnAdd.addEventListener("click",function(){
+    // panierMsg.innerHTML("test")
+    panier++;
+    p_elem.innerText = "Vous avez " +panier+ " produit dans votre panier."
+
+    // panierMsg.inneText = panier;
+    
+
+
+    console.log(panier)
+})
+
+
+
+
+
+
+
+vignettes.forEach(function(vignette){ 
+
+    vignette.addEventListener("click", function(){
+        const urlVignette = vignette.getAttribute("src");
+        fullImg.setAttribute("src", urlVignette)
+        console.log(url)
+        // console.log(test)
+    });
+});
+    // ...
