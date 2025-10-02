d4c4e7872631e5753d4c80e11db302129a082f01
hassani rhahim <rhahim12@gmail.com>
2025-10-02 09:16:14 +0200

    ajout du 6

--- 6 - toast/script.js
+++ 6 - toast/script.js
@@ -11,3 +11,37 @@
 const btn = document.querySelector("button");
 // 2. Je recupere le conteneur de toasts
 const ctnToasts = document.querySelector(".container-toasts");
+btn.addEventListener("click", function () {
+    
+    
+    
+    
+    const nouvelle_elem = document.createElement("div");
+    ctnToasts.appendChild(nouvelle_elem);
+    
+    nouvelle_elem.classList.add("toast")
+    const nouvelleBalise_elem = document.createElement("p");
+    nouvelleBalise_elem.innerText = "Votre fichier est enregistrer !"
+    nouvelle_elem.appendChild(nouvelleBalise_elem);
+    
+    const test = setTimeout(greting, 1000);
+
+    function greting (){
+        console.log("salut")
+        toast.remove
+    }
+
+
+
+    
+
+
+    
+    
+    
+    
+    
+    
+    // console.log(phrase)
+    
+});
